79f142bf7c2c4f8d8126c6dd4f6726fda34d2154
vadik <vadimtsudenko@gmail.com>
2023-08-31 06:36:56 -0700

    main corrections

--- FormulaQualificationApp/src/main/java/ua/com/foxminded/Main.java
+++ FormulaQualificationApp/src/main/java/ua/com/foxminded/Main.java
@@ -4,17 +4,16 @@ import ua.com.foxminded.qualification.RaceDispaly;
 import ua.com.foxminded.qualification.RacerService;
 import java.io.IOException;
 
-
 public class Main {
     public static void main(String[] args) {
         RacerService raceService = new RacerService();
         RaceDispaly raceDispaly = new RaceDispaly();
         try {
-            System.out.println(raceDispaly.showFirstQualificationResult(raceService.getRacers("start.log", "end.log","abbreviations.txt")));
+            System.out.println(raceDispaly
+                    .showFirstQualificationResult(raceService.getRacers("start.log", "end.log", "abbreviations.txt")));
         } catch (IOException ioException) {
             System.err.print(ioException.getMessage());
             ioException.printStackTrace();
         }
     }
 }
-
